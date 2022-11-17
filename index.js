49249ca6c3a38f093400de21fa9d26ee1a9e899d
joanne muthoni <joanne.mumbi@student.moringaschool.com>
2022-11-17 23:19:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+let cats=['Milo', 'Otis', 'Garfield'];
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+    let joannesCats = [...cats];
+    joannesCats.push(name);
+    return joannesCats;
+}
+function prependCat(name){
+    let joannesCats = [...cats];
+    joannesCats.unshift(name);
+    return joannesCats;
+}
+function removeLastCat(){
+    let joannesCats = [...cats];
+    joannesCats.pop();
+    return joannesCats;
+}
+function removeFirstCat(){
+    let joannesCats = [...cats];
+    joannesCats.shift();
+    return joannesCats;
+}
